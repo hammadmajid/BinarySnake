a04e7c35d4b99a506ed2584158c50496bf9d4710
Hammad Majid <hammadmajid@proton.me>
2023-12-21 21:15:49 +0500

    draw 30x30 grid for the player to move into

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,48 @@
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "Binary Snake" };
+int main() {
+    constexpr unsigned int windowWidth = 1920u;
+    constexpr unsigned int windowHeight = 1080u;
+    constexpr unsigned int gridSize = 30u;
+
+    auto window = sf::RenderWindow{{windowWidth, windowHeight}, "Binary Snake"};
     window.setFramerateLimit(144);
 
-    while (window.isOpen())
-    {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
+    // Create a 2D array to represent the grid
+    std::vector<std::vector<sf::RectangleShape>> grid;
+
+    // Initialize the grid with rectangles
+    for (unsigned int i = 0; i < gridSize; i++) {
+        std::vector<sf::RectangleShape> row;
+        for (unsigned int j = 0; j < gridSize; j++) {
+            constexpr unsigned int cellSize = 20u;
+            sf::RectangleShape cell(sf::Vector2f(cellSize, cellSize));
+            cell.setPosition(static_cast<float>(i) * cellSize, static_cast<float>(j) * cellSize);
+            cell.setFillColor(sf::Color::Black);
+            cell.setOutlineColor(sf::Color::White);
+            cell.setOutlineThickness(1.0f);
+            row.emplace_back(cell);
+        }
+        grid.emplace_back(row);
+    }
+
+
+    while (window.isOpen()) {
+        for (auto event = sf::Event{}; window.pollEvent(event);) {
+            if (event.type == sf::Event::Closed) {
                 window.close();
             }
         }
 
         window.clear();
+
+        // Draw the grid
+        for (const auto&row: grid) {
+            for (const auto&cell: row) {
+                window.draw(cell);
+            }
+        }
+
         window.display();
     }
 }
