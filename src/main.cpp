8b309a3eb92688e08fc9e4e59a011e5ab96e0491
Hammad Majid <hammadmajid@proton.me>
2023-12-22 21:42:42 +0500

    move grid setup code to its own function

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,15 @@
 #include <SFML/Graphics.hpp>
+#include "graphics/grid.h"
 
 int main() {
     constexpr unsigned int windowWidth = 1366u;
     constexpr unsigned int windowHeight = 768u;
-    constexpr unsigned int gridSize = 30u;
 
     auto window = sf::RenderWindow{{windowWidth, windowHeight}, "Binary Snake"};
     window.setFramerateLimit(144);
 
-    constexpr unsigned int cellSize = 20u;
-    constexpr unsigned int gridWidth = gridSize * cellSize;
-    constexpr unsigned int gridHeight = gridSize * cellSize;
-
-    // Calculate the starting position to center the grid
-    float startX = (windowWidth - gridWidth) / 2.0f;
-    float startY = (windowHeight - gridHeight) / 2.0f;
-
     // Create a 2D array to represent the grid
-    std::vector<std::vector<sf::RectangleShape>> grid;
-
-    // Initialize the grid with rectangles
-    for (unsigned int i = 0; i < gridSize; i++) {
-        std::vector<sf::RectangleShape> row;
-        for (unsigned int j = 0; j < gridSize; j++) {
-            sf::RectangleShape cell(sf::Vector2f(cellSize, cellSize));
-            cell.setPosition(startX + static_cast<float>(i) * cellSize, startY + static_cast<float>(j) * cellSize);
-            cell.setFillColor(sf::Color::Black);
-            cell.setOutlineColor(sf::Color::White);
-            cell.setOutlineThickness(1.0f);
-            row.emplace_back(cell);
-        }
-        grid.emplace_back(row);
-    }
-
+   auto grid = setup_grid(windowWidth, windowHeight);
 
     while (window.isOpen()) {
         for (auto event = sf::Event{}; window.pollEvent(event);) {
